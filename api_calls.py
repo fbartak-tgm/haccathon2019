eb4d159310e833b51a113f4853bc892a34539bfc
lhalbritter-tgm <lhalbritter@student.tgm.ac.at>
2019-01-30 11:11:00 +0100

    Now you can get every movie in city 'Wien' by genre :)

--- api_calls.py
+++ api_calls.py
@@ -1,5 +1,13 @@
 import requests as rq
 from pprint import pprint
 
-r = rq.get("https://efs.film.at/api/v1/cfs/filmat/screenings/nested/movie/2019-01-31?city=Wien")
-pprint(r)
+filme = rq.get("https://efs.film.at/api/v1/cfs/filmat/screenings/nested/movie/2019-01-30?city=Wien").json()["result"]
+
+
+def get_film_by_genre(genre):
+    results = []
+    for film in filme:
+        if "genres" in film["parent"]:
+            if genre in film["parent"]["genres"]:
+                results.append(film["parent"]["title"])
+    return results
